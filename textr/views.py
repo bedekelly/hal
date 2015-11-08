22e87169befa005b5747108427bb2a8653bcc974
Bede Kelly <bedekelly@yahoo.com>
2015-11-08 02:33:01 -0500

    Add an *actual* debug output when our API gets hit

--- textr/views.py
+++ textr/views.py
@@ -6,4 +6,5 @@ from .generator import gen_response
 @app.route("/api/v1.0/received", methods=["GET", "POST"])
 def recieved():
     message = get_last_message()
+    print("Message received:", message)
     return twiml_response(gen_response(message))
